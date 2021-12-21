27481cfbf0d1dab976c2e465ea2ccc9054b026f8
WEI <wei12f8158@gmail.com>
2021-12-21 15:28:42 +0800

    Added playsound.

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -1,14 +1,25 @@
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound(soundName: (sender.titleLabel?.text)!)
+        //Connected to every key.
+        
+    }
     
-
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
-
